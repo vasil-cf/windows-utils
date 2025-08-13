e04398b13c1a61ee2e4994f26740bedea65503e5
Vasil Sudakou <160465134+vasil-cf@users.noreply.github.com>
2025-08-13 06:09:25 +0300

    Update troubleshoot-pull.ps1

--- troubleshoot-pull.ps1
+++ troubleshoot-pull.ps1
@@ -1,37 +1,50 @@
 #Requires -RunAsAdministrator
+Set-StrictMode -Version Latest
 $ErrorActionPreference = 'Stop'
 
 function Wait-DockerUp {
-  for ($i=0; $i -lt 30; $i++) {
+  for ($i = 0; $i -lt 30; $i++) {
     try { docker info | Out-Null; if ($LASTEXITCODE -eq 0) { return } } catch {}
     Start-Sleep -Seconds 1
   }
   throw "Docker daemon didn't come up in time."
 }
 
-# Enable debug
-sc.exe stop docker | Out-Null
-sc.exe config docker binPath= "C:\Windows\system32\dockerd.exe --run-service --service-name docker --debug" | Out-Null
-sc.exe start docker | Out-Null
-Wait-DockerUp
-docker info | Select-String 'Debug Mode' | Write-Host
+# Enable per-line tracing (PowerShell 5.1). If you're on PS7+, this isn't supported.
+$SupportsSetPSDebug = $PSVersionTable.PSVersion.Major -lt 7
+if ($SupportsSetPSDebug) { Set-PSDebug -Trace 1 }
 
-# Repro: clean + pull (ignore if image isn't present)
-docker image rm quay.io/codefresh/cf-git-cloner:windows-21H2 2>$null
-docker pull quay.io/codefresh/cf-git-cloner:windows-21H2
-
-# Show full, untruncated daemon events for last 30 minutes
-Get-WinEvent -FilterHashtable @{LogName='Application'; ProviderName='docker'; StartTime=(Get-Date).AddMinutes(-30)} |
-  Sort-Object TimeCreated |
-  Select-Object TimeCreated, Id, LevelDisplayName, @{n='Message'; e={$_.Message}} |
-  Format-List | Out-String -Width 8192 | Write-Output
-
-# Always revert service to non-debug
 try {
+  # --- Enable daemon debug mode ---
   sc.exe stop docker | Out-Null
-  sc.exe config docker binPath= "C:\Windows\system32\dockerd.exe --run-service --service-name docker" | Out-Null
+  sc.exe config docker binPath= "C:\Windows\system32\dockerd.exe --run-service --service-name docker --debug" | Out-Null
   sc.exe start docker | Out-Null
   Wait-DockerUp
-} finally {
+
   docker info | Select-String 'Debug Mode' | Write-Host
+
+  # --- Reproduce: remove image (ignore if absent) + pull ---
+  docker image rm quay.io/codefresh/cf-git-cloner:windows-21H2 2>$null
+  docker pull quay.io/codefresh/cf-git-cloner:windows-21H2
+
+  # --- Dump full, untruncated daemon events for last 30 minutes ---
+  Get-WinEvent -FilterHashtable @{ LogName='Application'; ProviderName='docker'; StartTime=(Get-Date).AddMinutes(-30) } |
+    Sort-Object TimeCreated |
+    Select-Object TimeCreated, Id, LevelDisplayName, @{ n='Message'; e={ $_.Message } } |
+    Format-List | Out-String -Width 8192 | Write-Output
+}
+finally {
+  # --- Always revert the service back to non-debug ---
+  try {
+    sc.exe stop docker | Out-Null
+    sc.exe config docker binPath= "C:\Windows\system32\dockerd.exe --run-service --service-name docker" | Out-Null
+    sc.exe start docker | Out-Null
+    Wait-DockerUp
+
+    docker info | Select-String 'Debug Mode' | Write-Host
+  } catch {
+    Write-Warning "Failed to revert docker service: $($_.Exception.Message)"
+  }
+
+  if ($SupportsSetPSDebug) { Set-PSDebug -Trace 0 }
 }
